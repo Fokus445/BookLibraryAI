871f16f00c6aa29b480ea837fee81cc18ef619de
Fokus445 <79461074+Fokus445@users.noreply.github.com>
2023-09-30 19:04:51 +0300

    chore: script to add data

--- core_apps/recommender/admin.py
+++ core_apps/recommender/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import ***REMOVED***
 
+from .models import Book, Author, Publisher
 # Register your models here.
+
+class BookAdmin(***REMOVED***.ModelAdmin):
+    list_display = ["isbn","title","description"]
+
+***REMOVED***.site.register(Book)
+***REMOVED***.site.register(Author)
+***REMOVED***.site.register(Publisher)
