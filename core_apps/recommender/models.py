a85cbded74970ca13964a8e11539d295ac386ea5
Fokus445 <79461074+Fokus445@users.noreply.github.com>
2023-10-01 19:09:58 +0300

    chore: import_data script, recommend serializers

--- core_apps/recommender/models.py
+++ core_apps/recommender/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 User = get_user_model()
 
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=30, verbose_name=_("first name"))
-    last_name = models.CharField(max_length=30, verbose_name=_("last name"))
+    first_name = models.CharField(max_length=50, verbose_name=_("first name"))
+    last_name = models.CharField(max_length=50, verbose_name=_("last name"))
 
     class Meta:
         ordering = ["first_name","last_name"]
@@ -28,14 +29,21 @@ class Publisher(models.Model):
 
 
 class Book(models.Model):
-    isbn = models.BigIntegerField(max_length=13)
+    isbn = models.CharField(max_length=17, unique=True, verbose_name="ISBN")
     title = models.CharField(verbose_name=_("book title"), max_length=200)
-    description = models.CharField(verbose_name=_("description"), max_length=255)
+    description = models.CharField(verbose_name=_("description"), max_length=255, blank=True)
     author = models.ForeignKey(Author, related_name="books",verbose_name=_("author"),
         on_delete=models.CASCADE)
     publisher = models.ForeignKey(Publisher, related_name="published_books", 
-        verbose_name=_("publisher"), on_delete=models.DO_NOTHING, blank=True)
-    release_date = models.DateTimeField(verbose_name=_("release date"))
+        verbose_name=_("publisher"), on_delete=models.DO_NOTHING, null=True, blank=True)
+    release_year = models.IntegerField(
+        verbose_name=_("release_year"),
+        blank=True,
+        null=True,
+        validators=[
+            MinValueValidator(1000),
+            MaxValueValidator(9999)
+        ])
     cover_image = models.CharField(max_length=200)
 
     class Meta:
