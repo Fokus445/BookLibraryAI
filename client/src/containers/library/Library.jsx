892a4a09c4d4b81bbf79bed09fa6590794d7d568
Fokus445 <79461074+Fokus445@users.noreply.github.com>
2023-10-04 17:40:38 +0300

    chore: container and component setup for client

---
+++ client/src/containers/library/Library.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import './library.css';
+
+function Library() {
+
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+      // Replace 'apiUrl' with the actual URL of your API
+      axios.get('/books')
+        .then((response) => response.json())
+        .then((data) => setBooks(data))
+        .catch((error) => console.error('Error fetching data:', error));
+    }, []);
+    
+
+    return (
+        <div className='library'>
+            test
+        </div>
+    )
+}
+
+export default Library;
