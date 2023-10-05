2e20355ee84b8078b2228501f487d0c6aae0f31c
Fokus445 <79461074+Fokus445@users.noreply.github.com>
2023-10-05 09:06:20 +0300

    fix: can't connect to client:3000 issue

--- books_api/settings/local.py
+++ books_api/settings/local.py
@@ -9,7 +9,15 @@ SECRET_KEY = env(
 
 DEBUG = True
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080, http://localhost:3000"]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Add your React app's origin here
+]
+
+ALLOWED_HOSTS = ['*']
+
+
 
 EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
 EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
@@ -17,3 +25,6 @@ EMAIL_PORT = env("EMAIL_PORT")
 DEFAULT_FROM_EMAIL = "support@edwin.com"
 DOMAIN = env("DOMAIN")
 SITE_NAME = "Book Recommender"
+
+
+
