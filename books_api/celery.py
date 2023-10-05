3bae2bd889907e99d4f73df83fcd391b20f63e0c
Fokus445 <79461074+Fokus445@users.noreply.github.com>
2023-10-05 22:18:04 +0300

    chore: production settings

--- books_api/celery.py
+++ books_api/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 from django.conf import settings
 
 # TODO: change this in production
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "books_api.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "books_api.settings.production")
 
 app = Celery("books_api")
 
