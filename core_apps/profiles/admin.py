c01dbaae928d72ea8e02faee74445995e80fde0e
Edwin <edwinshaman@gmail.com>
2023-10-18 13:52:17 +0300

    chore: flush migrations

--- core_apps/profiles/admin.py
+++ core_apps/profiles/admin.py
@@ -4,9 +4,9 @@ from .models import Profile
 
 
 class ProfileAdmin(***REMOVED***.ModelAdmin):
-    list_display = ["pkid", "id", "user", "gender"]
-    list_display_links = ["pkid", "id", "user"]
-    list_filter = ["id", "pkid"]
+    list_display = ["id", "uuid", "user", "gender"]
+    list_display_links = ["id", "uuid", "user"]
+    list_filter = ["id", "uuid"]
 
 
 ***REMOVED***.site.register(Profile, ProfileAdmin)
